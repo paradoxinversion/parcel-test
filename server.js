a7564cd9641407c77721af613476de3038a59338
Jedai Saboteur <dev@paradoxinversion.com>
2018-06-30 16:03:34 -0700

    Added Procfile; Made server port respect env

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res) => res.send("Hello World!"));
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.resolve(__dirname, "dist")));
   app.get("*", function(req, res) {
     res.sendFile(path.resolve(__dirname, "dist/index.html"));
   });
 }
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Example app listening on port 3000!")
+);
